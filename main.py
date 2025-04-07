df319c57b412d52a6d9fed01d05aeb9ead897505
chocman5 <lochiemack@hotmail.com>
2025-04-07 12:52:30 +1000

    Print a Report - Completed

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
+#python 3
+
 from stats import get_num_words
 from stats import count_characters
+from stats import sort_characters_by_count
+from stats import print_characters
+
+def header(path):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+
 
 def main():
-    file_contents = get_num_words()
-    count_characters(file_contents)
+
+    # Set variables
+
+    book_path = "books/frankenstein.txt"
+
+    # Header Text 
+    header(book_path)
+    print ("----------- Word Count ----------") # display formating
+
+    # prints and returns number of words
+    file_contents = get_num_words(book_path) 
+
+    print("--------- Character Count -------")
+    char_count = count_characters(file_contents) # builds directory of characters
+    char_list = sort_characters_by_count(char_count) #builds list of directories. Sorts by count
+
+    print_characters(char_list)
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,9 @@
-def get_num_words():
-    with open("/home/lochie/workspace/github.com/chocman5/bookbot/books/frankenstein.txt") as f:
+def get_num_words(path):
+    with open(path) as f:
         file_contents = f.read()
         words = file_contents.split()
         num_words = len(words)
-        print(f"{num_words} words found in the document")
+        print(f"Found {num_words} total words")
         return file_contents
 
 def count_characters(text):
@@ -19,4 +19,31 @@ def count_characters(text):
         else:
             char_count[character] = 1
     
-    print(char_count)
+    return char_count
+
+def sort_on(dict):
+    return dict["count"]  
+
+def sort_characters_by_count(char_count):
+    # Create a list to hold dictionaries
+    chars_list = []
+    
+    # For each character in the character count dictionary
+    for char, count in char_count.items():
+        # Create a dictionary for this character and its count
+        char_dict = {"character": char, "count": count}
+        # Add the dictionary to our list
+        chars_list.append(char_dict)
+    
+    # Sort the list by count (greatest to least)
+    chars_list.sort(reverse=True, key=sort_on)
+    
+    return chars_list
+
+def print_characters(char_list):
+     for char_dict in char_list:
+        character = char_dict["character"]
+        count = char_dict["count"]
+        
+        if character.isalpha():
+            print(f"{character}: {count}")
